43def3c45e4b78f230e44fe96d5987ece4fadbac
Nathan Wolf <nwolf2@oswego.edu>
2023-04-25 19:16:47 -0400

    redo of main menu screen

--- src/main/java/com/csc445cowboys/guiwip/MainLobbyController.java
+++ src/main/java/com/csc445cowboys/guiwip/MainLobbyController.java
@@ -6,19 +6,5 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class MainLobbyController {
-    public TableColumn lobby_enter_tbl_button;
-    public TableColumn lobby_col_room;
-    public TableColumn lobby_col_status;
-    public TableColumn lobby_col_curr_players;
-    public TableColumn lobby_col_max_players;
-    public TableView lobby_table;
-    public Button credits;
-    public Button button_create_lobby;
 
-    public void onClickShowCredits(ActionEvent actionEvent) {
-
-    }
-
-    public void onClickCreateLobby(ActionEvent actionEvent) {
-    }
 }
