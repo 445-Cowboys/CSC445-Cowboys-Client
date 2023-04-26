e7860ca283d591dc757b6e9a5dc9f1ffb08bbbb9
Nathan Wolf <nwolf2@oswego.edu>
2023-04-25 22:07:04 -0400

    redid main fxml, added methods and buttons to controller

--- src/main/java/com/csc445cowboys/guiwip/MainLobbyController.java
+++ src/main/java/com/csc445cowboys/guiwip/MainLobbyController.java
@@ -2,9 +2,26 @@ package com.csc445cowboys.guiwip;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class MainLobbyController {
 
+    public Label lobby1_curr_players_label;
+    public Label lobby1_game_status_label;
+    public Label lobby2_curr_players_label1;
+    public Label lobby2_game_status_label;
+    public Label lobby3_game_status_label;
+    public Label lobby3_curr_players_label;
+
+
+    public void onLobby1EnterGame(ActionEvent actionEvent) {
+    }
+
+    public void onLobby2EnterGame(ActionEvent actionEvent) {
+    }
+
+    public void onLobby3EnterGame(ActionEvent actionEvent) {
+    }
 }
