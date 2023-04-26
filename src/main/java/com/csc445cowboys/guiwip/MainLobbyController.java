76ad0e3bed3da54b98dc4a2ca63ca3f590a552af
Nathan Wolf <nwolf2@oswego.edu>
2023-04-25 22:14:28 -0400

    added set methods for lobbys

--- src/main/java/com/csc445cowboys/guiwip/MainLobbyController.java
+++ src/main/java/com/csc445cowboys/guiwip/MainLobbyController.java
@@ -1,10 +1,7 @@
 package com.csc445cowboys.guiwip;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 
 public class MainLobbyController {
 
@@ -17,11 +14,29 @@ public class MainLobbyController {
 
 
     public void onLobby1EnterGame(ActionEvent actionEvent) {
+        System.out.println("Lobby 1 Enter Game");
     }
 
     public void onLobby2EnterGame(ActionEvent actionEvent) {
+        System.out.println("Lobby 2 Enter Game");
     }
 
     public void onLobby3EnterGame(ActionEvent actionEvent) {
+        System.out.println("Lobby 3 Enter Game");
+    }
+
+    public void setLobby1(int curr_players, String game_status) {
+        lobby1_curr_players_label.setText(String.valueOf(curr_players));
+        lobby1_game_status_label.setText(game_status);
+    }
+
+    public void setLobby2(int curr_players, String game_status) {
+        lobby2_curr_players_label1.setText(String.valueOf(curr_players));
+        lobby2_game_status_label.setText(game_status);
+    }
+
+    public void setLobby3(int curr_players, String game_status) {
+        lobby3_curr_players_label.setText(String.valueOf(curr_players));
+        lobby3_game_status_label.setText(game_status);
     }
 }
