8e419ba99f05daccc7258345317e36284cbacea3
daltamur <daltamur@oswego.edu>
2023-05-11 11:16:48 -0400

    modified server liveness update

---
+++ src/main/java/com/csc445cowboys/guiwip/packets/ServerLivenessUpdate.java
@@ -0,0 +1,20 @@
+package com.csc445cowboys.guiwip.packets;
+
+import java.nio.ByteBuffer;
+
+public class ServerLivenessUpdate {
+    private final int[] serverStates=new int[3];
+    private final long timeStamp;
+
+    public ServerLivenessUpdate(ByteBuffer buf){
+        serverStates[0] = buf.getInt(2);
+        serverStates[1] = buf.getInt(6);
+        serverStates[2] = buf.getInt(10);
+        timeStamp = buf.getLong(14);
+    }
+
+    public int[] getServerStates(){return serverStates;}
+
+    public long getTimeStamp(){return timeStamp;}
+
+}
