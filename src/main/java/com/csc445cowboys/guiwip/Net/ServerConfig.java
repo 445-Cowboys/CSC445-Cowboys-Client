db7bdb73e3eae26f5ffb275d29b2c9f931e0b49e
daltamur <daltamur@oswego.edu>
2023-05-10 22:01:25 -0400

    testing with servers again

--- src/main/java/com/csc445cowboys/guiwip/Net/ServerConfig.java
+++ src/main/java/com/csc445cowboys/guiwip/Net/ServerConfig.java
@@ -3,7 +3,6 @@ package com.csc445cowboys.guiwip.Net;
 public final class ServerConfig {
 
     public static final String[] SERVER_NAMES = {
-            "localhost",
             "rho.cs.oswego.edu",
             "moxie.cs.oswego.edu",
             "altair.cs.oswego.edu",
